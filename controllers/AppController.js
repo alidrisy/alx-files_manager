4b76180765a24cf8b6f99a63e939f5ff5a60fdf7
alidrisy <a.alidrisy6@gmail.com>
2024-03-09 19:38:56 +0300

    fix errors

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,16 +2,16 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static getStatus (_req, res) {
+  static getStatus(_req, res) {
     return res.send({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
-  };
+  }
 
-  static getStats async (_req, res) {
+  static async getStats(_req, res) {
     return res.send({
       users: await dbClient.nbUsers(),
       files: await dbClient.nbFiles(),
     });
-  };
+  }
 }
 
 export default AppController;
