6d0fa37e90013e4a65f0121d52ee6b72ac740da5
alidrisy <a.alidrisy6@gmail.com>
2024-03-09 19:37:30 +0300

    4. Authenticate a user

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,11 +2,11 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static getStatus = (_req, res) => {
+  static getStatus (_req, res) {
     return res.send({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   };
 
-  static getStats = async (_req, res) => {
+  static getStats async (_req, res) {
     return res.send({
       users: await dbClient.nbUsers(),
       files: await dbClient.nbFiles(),
--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ const routes = express.Router();
 routes.get('/status', AppController.getStatus);
 routes.get('/stats', AppController.getStats);
 routes.post('/users', UsersController.postNew);
+routes.get('/users/me', UsersController.getMe);
 routes.get('/connect', AuthController.getConnect);
 routes.get('/disconnect', AuthController.getDisconnect);
-routes.get('/users/me', UsersController.getMe);
 
 export default routes;
