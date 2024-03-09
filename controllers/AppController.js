6f04ca9318f975ea75984c9df5405c255a807d94
alidrisy <a.alidrisy6@gmail.com>
2024-03-09 20:09:13 +0300

    fix errors

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,22 +1,18 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-class AppController {
-  static getStatus(request, response) {
-    const status = {
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    };
-    response.status(200).send(status);
-  }
+export const getStatus = (request, response) => {
+  const status = {
+    redis: redisClient.isAlive(),
+    db: dbClient.isAlive(),
+  };
+  response.status(200).send(status);
+};
 
-  static async getStats(request, response) {
-    const stats = {
-      users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles(),
-    };
-    response.status(200).send(stats);
-  }
-}
-
-export default AppController;
+export const getStats = async (request, response) => {
+  const stats = {
+    users: await dbClient.nbUsers(),
+    files: await dbClient.nbFiles(),
+  };
+  response.status(200).send(stats);
+};
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import AppController from '../controllers/AppController';
+import { getStats, getStatus } from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 
 const router = express.Router();
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
+router.get('/status', getStatus);
+router.get('/stats', getStats);
 router.post('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
 router.get('/connect', AuthController.getConnect);
