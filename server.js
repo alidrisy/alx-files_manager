7d552393fd6535faf7b629f7454f73ff5e6b797d
alidrisy <a.alidrisy6@gmail.com>
2024-03-09 20:57:05 +0300

    3. Create a new user

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use(router);
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Server running in localhost:${port}`);
